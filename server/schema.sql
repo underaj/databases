ae3e9b5dd8f6748e8ee620b0b6fe83cd24009f57
underaj <chingyang91@gmail.com>
2016-07-01 19:56:21 -0700

    use schema to create chat database

--- server/schema.sql
+++ server/schema.sql
@@ -2,9 +2,23 @@ CREATE DATABASE chat;
 
 USE chat;
 
+CREATE TABLE users (
+  /* Describe your table here.*/
+  u_id INT NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (u_id)
+);
+
 CREATE TABLE messages (
   /* Describe your table here.*/
+  m_id INT NOT NULL AUTO_INCREMENT,
+  message VARCHAR(100) NOT NULL,
+  user_id INT NOT NULL,
+  PRIMARY KEY (m_id),
+  FOREIGN KEY (user_id) REFERENCES users(u_id)
 );
+
+
+
 
 /* Create other tables and define schemas for them here! */
 
